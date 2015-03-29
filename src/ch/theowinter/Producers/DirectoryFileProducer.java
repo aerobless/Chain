51560df1d8e4a7166d646dfe1a3bd70c91d1ba68
aerobless <theo.winter@gmail.com>
2015-03-29 15:31:30 +0200

    Improved DirectoryFileProducer. It now reads the creationDate from the filename if specified as "yyyy-mm-dd", if not specified it attempts to read the creation date from the files metadata. But be aware that not all filesystems store the creationDate.. so this might not work for all operating systems. (Tested to work on OS X HFS)

--- src/ch/theowinter/Producers/DirectoryFileProducer.java
+++ src/ch/theowinter/Producers/DirectoryFileProducer.java
@@ -10,6 +10,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -23,7 +24,7 @@ public class DirectoryFileProducer implements Chain {
         this.filetype = filetype;
     }
 
-    public List<ChainEvent> getFolderContent(String path) throws ParseException, IOException {
+    private List<ChainEvent> getFolderContent() throws ParseException, IOException {
         File folder = new File(path);
         File[] listOfFiles = folder.listFiles();
 
@@ -31,20 +32,37 @@ public class DirectoryFileProducer implements Chain {
 
         for (int i = 0; i < listOfFiles.length; i++) {
             if (listOfFiles[i].isFile() && listOfFiles[i].getName().contains(filetype)) {
-                Path pPath = Paths.get(listOfFiles[i].getAbsolutePath());
-                System.out.println(listOfFiles[i].getAbsolutePath());
-                BasicFileAttributes attributes = Files.readAttributes(pPath, BasicFileAttributes.class);
+                Date creationDate;
 
-                System.out.println(new Date(attributes.creationTime().toMillis()));
-                results.add(new ChainEvent("File with Type "+filetype, listOfFiles[i].getAbsolutePath(), new Date(attributes.creationTime().toMillis())));
+                if(filenameContainsDate(listOfFiles[i])){
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                    creationDate = sdf.parse(listOfFiles[i].getName().substring(0,10));
+                } else {
+                    Path pPath = Paths.get(listOfFiles[i].getAbsolutePath());
+                    BasicFileAttributes attributes = Files.readAttributes(pPath, BasicFileAttributes.class);
+                    creationDate = new Date(attributes.creationTime().toMillis());
+                }
+                results.add(new ChainEvent("File with Type "+filetype, listOfFiles[i].getAbsolutePath(), creationDate));
             }
         }
         return results;
     }
 
+    private boolean filenameContainsDate(File listOfFile) {
+        return listOfFile.getName().matches("^\\d{4}-\\d\\d-\\d\\d [\\s\\S]*");
+    }
+
+    /**
+     * Generates a list of ChainEvents containing the filetype, path and creation date of files in a
+     * specific folder. If the filename starts with a date of the following format: yyyy-mm-dd that date is used
+     * as creation date. If the filename does not contain a date DirectoryFileProducer attempts to read the
+     * creationDate from the file-meta-data. Be aware that not all filesystems store the file-creation date and so
+     * this may not work for you. Tested on HFS, OS X.
+     * @return
+     */
     public List<ChainEvent> compileChain(){
         try {
-            return getFolderContent(path);
+            return getFolderContent();
         } catch (ParseException e) {
             e.printStackTrace();
         } catch (IOException e) {
